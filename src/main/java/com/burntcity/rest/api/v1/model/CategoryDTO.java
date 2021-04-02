e995419095ac8dd124f854552e5c40f35abb41e4
Miguel Moreo <miguelangel.moreo@gmail.com>
2021-04-02 12:40:21 +0200

    Fixed CategoryDTO --> DATA

--- src/main/java/com/burntcity/rest/api/v1/model/CategoryDTO.java
+++ src/main/java/com/burntcity/rest/api/v1/model/CategoryDTO.java
@@ -1,9 +1,12 @@
 package com.burntcity.rest.api.v1.model;
 
+import lombok.Data;
+
 /**
  * We are going to expose CategoryDTO through our REST controller
  *
  */
+@Data
 public class CategoryDTO {
 	
 	private Long Id;
